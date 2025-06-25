2f930d9a5a9b8f02fe205649b97794e06a37e141
Ethan McLaughlin <essence102405@gmail.com>
2025-06-25 14:01:36 -0700

    feat: Installed mysql2 (to connecet to the db) and sequelize for interacting with the db.

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+import mysql2 from "mysql2"
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+
+});
+
+export default sequelize;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config.js';
 import cors from 'cors';
+import sequelize from './config/database.js';
 
 const PORT = process.env.PORT || '8080';
 const URL_CLIENT = process.env.URL_CLIENT || 'http://localhost:5173';
@@ -12,6 +13,19 @@ app.listen(PORT, () => {
     console.log('App is listening on port ' + PORT);
 });
 
+    // TESTING DB FUNCTIONALITY
+async function testDatabaseConnection() {
+  try {
+    await sequelize.authenticate(); // Attempt to authenticate the connection
+    console.log('Database connection has been established successfully.'); // Log success if connected
+  } catch (error) {
+    console.error('Unable to connect to the database:', error); // Log error if connection fails
+  }
+}
+
+testDatabaseConnection(); 
+
+
 // configure cors
 const corsOptions = {
     origin: ['http://localhost:5173', 'http://localhost:3000'],
@@ -20,6 +34,12 @@ const corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
+
+    //Middle Ware
+// Add to index.js after cors
+app.use(express.json()); //Parses incoming request with JSON payloads and available in req.body
+app.use(express.urlencoded({ extended: true })); // parses the data from URL forms and makes it available in "req.body"
+
 
 // @TODO: move this to a reasonable routes file; just for test now
 app.get('/', async (request, response) => {
@@ -30,3 +50,5 @@ app.get('/', async (request, response) => {
         response.status(500).send('System Error');
     }
 });
+
+
