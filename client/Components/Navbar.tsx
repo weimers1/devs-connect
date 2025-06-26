9abfea35a936c063746a83ed5021e55401b51407
Sam Weimer <samweimer7@gmail.com>
2025-06-26 09:38:17 -0400

    refactor: more navbar tweaks

--- client/Components/Navbar.tsx
+++ client/Components/Navbar.tsx
@@ -42,19 +42,21 @@ const Navbar: React.FC = () => {
 
     return (
         <nav className="bg-transparent relative">
-            <div className="flex items-center justify-between h-20 px-2 sm:px-6 lg:px-8">
+            <div className="flex grid grid-cols-2 md:grid-cols-3 px-8 pt-5">
                 {/* Logo */}
-                <img
-                    src={assets.Logo}
-                    className="w-14 h-15"
-                    alt="home"
-                />
+                <Link to="/">
+                    <img
+                        src={assets.Logo}
+                        className="w-14 h-15"
+                        alt="logo"
+                    />
+                </Link>
 
                 {/* Desktop Search Bar */}
-                <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2">
+                <div className="hidden md:block">
                     <input
                         type="text"
-                        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                        className="border-2 border-gray-300 bg-white h-10 w-100 px-5 mt-3 pr-16 rounded-lg text-sm focus:outline-none"
                         placeholder="Search...."
                     />
                 </div>
@@ -64,9 +66,9 @@ const Navbar: React.FC = () => {
                     {pages.map((page, i) => {
                         return (
                             <Link
-                                className="hover:bg-white text-white hover:text-blue-900 rounded-lg mx-5 p-3"
+                                className="hover:bg-white text-white hover:text-blue-900 rounded-lg mx-2 p-3"
                                 to={page.route}
-                                key={`link-${i}`}
+                                key={`desktop-link-${i}`}
                             >
                                 <div className="grid place-items-center">
                                     <Icon
@@ -83,55 +85,47 @@ const Navbar: React.FC = () => {
                 </div>
 
                 {/* Mobile Menu Button */}
-                <div className="md:hidden">
+                <div className="flex md:hidden">
                     <button
-                        className="p-2 rounded-md text-gray-600 hover:bg-gray-100"
+                        className="hover:bg-white text-white hover:text-blue-900"
                         onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                    ></button>
+                    >
+                        <Icon
+                            icon="mdi:menu"
+                            className="w-8 h-8"
+                        />
+                    </button>
                 </div>
             </div>
 
             {/* Mobile Menu Dropdown */}
             {isMobileMenuOpen && (
-                <div className="md:hidden absolute top-full left-0 right-0 bg-white border-b border-gray-200 shadow-lg z-50">
-                    <div className="px-4 py-2 space-y-2">
-                        <Link
-                            className="block py-2 text-gray-700 hover:bg-gray-100 rounded"
-                            to="/home"
-                            onClick={() => setIsMobileMenuOpen(false)}
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            className="block py-2 text-gray-700 hover:bg-gray-100 rounded"
-                            to="/communities"
-                            onClick={() => setIsMobileMenuOpen(false)}
-                        >
-                            Communities
-                        </Link>
-                        <Link
-                            className="block py-2 text-gray-700 hover:bg-gray-100 rounded"
-                            to="/messages"
-                            onClick={() => setIsMobileMenuOpen(false)}
-                        >
-                            Messages
-                        </Link>
-                        <Link
-                            className="block py-2 text-gray-700 hover:bg-gray-100 rounded"
-                            to="/profile"
-                            onClick={() => setIsMobileMenuOpen(false)}
-                        >
-                            Profile
-                        </Link>
+                <div className="md:hidden absolute top-full left-0 right-0 z-50">
+                    {/* Mobile Search */}
+                    <div className="pt-2 px-3">
+                        <input
+                            type="text"
+                            className="w-full bg-white border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:border-blue-500"
+                            placeholder="Search...."
+                        />
+                    </div>
 
-                        {/* Mobile Search */}
-                        <div className="pt-2">
-                            <input
-                                type="text"
-                                className="w-full border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:border-blue-500"
-                                placeholder="Search...."
-                            />
-                        </div>
+                    <div className="px-4 py-2 space-y-2">
+                        {pages.map((page, i) => {
+                            return (
+                                <Link
+                                    key={`mobile-link-${i}`}
+                                    className="flex py-2 text-white"
+                                    to={page.route}
+                                >
+                                    <Icon
+                                        icon={page.icon}
+                                        className="w-6 h-6"
+                                    />
+                                    <span className="ps-2">{page.title}</span>
+                                </Link>
+                            );
+                        })}
                     </div>
                 </div>
             )}
